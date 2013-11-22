0be10a9582fa4eb2e97de2912a1c3a85d9b5c766
Calvin Metcalf <cmetcalf@appgeo.com>
2013-11-21 20:12:36 -0500

    promises and such

--- index.js
+++ index.js
@@ -1,157 +1,147 @@
-
+var promise = require('lie');
+var denodify('lie-denodify');
 // If we wanted to store incremental views we can do it here by listening
 // to the changes feed (keeping track of our last update_seq between page loads)
 // and storing the result of the map function (possibly using the upcoming
 // extracted adapter functions)
+var isArray = Array.isArray || function(obj) {
+  return type(obj) === "array";
+};
 
-function Spatial(db) {
-
-  var isArray = Array.isArray || function(obj) {
-    return type(obj) === "array";
-  };
-  
-  function viewQuery(fun, options) {
-    if (!options.complete) {
-      return;
-    }
-
-    var results = [];
-    var current = null;
-    var num_started= 0;
-    var completed= false;
-
-    // Make the key a proper one. If a value is a single point, transform it
-    // to a range. If the first element (or the whole key) is a geometry,
-    // calculate its bounding box.
-    // The geometry is also returned (`null` if there is none).
-    function normalizeKey(key) {
-      var newKey = [];
-      var geometry = null;
+function normalizeKey(key) {
+  var newKey = [];
+  var geometry = null;
 
       // Whole key is one geometry
-      if (!isArray(key) && typeof key === "object") {
-        return {
-          key: Spatial.calculateBbox(key),
-          geometry: key
+  if (!isArray(key) && typeof key === "object") {
+    return {
+      key: Spatial.calculateBbox(key),
+      geometry: key
+    };
+  }
+
+  if (!isArray(key[0]) && typeof key[0] === "object") {
+    newKey = Spatial.calculateBbox(key[0]);
+    geometry = key[0];
+    key = key.slice(1);
+  }
+
+ for(var i=0; i<key.length; i++) {
+    if(isArray(key[i])) {
+        newKey.push(key[i]);
+      // If only a single point, not a range was emitted
+    } else {
+      newKey.push([key[i], key[i]]);
+    }
+  }
+  return {
+    key: newKey,
+    geometry: geometry
+  };
+};
+function within(key, start_range, end_range) {
+  var start;
+  var end;
+
+  for(var i=0; i<key.length; i++) {
+    start = key[i][0];
+    end = key[i][1];
+    if (
+      ((start_range[i] === null && (start <= end_range[i] || end_range[i] === null))
+       // Start is set
+        || (start <= end_range[i] || end_range[i] === null))
+        &&
+        // Wildcard at the end
+       ((end_range[i] === null && (end >= start_range[i] || start_range[i] === null))
+        // End is set
+        || (end >= start_range[i] || start_range[i] === null))) {
+        continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+function Spatial(db) {
+  var get = denodify(db.get);
+  function viewQuery(fun, options) {
+    return promise(function(success,failure){
+      var results = [];
+      var current = null;
+      var num_started= 0;
+      var completed= false;
+
+      function emit(key, val) {
+        var keyGeom = normalizeKey(key);
+        var viewRow = {
+          id: current.doc._id,
+          key: keyGeom.key,
+          value: val,
+          geometry: keyGeom.geometry
         };
-      }
 
-      if (!isArray(key[0]) && typeof key[0] === "object") {
-        newKey = Spatial.calculateBbox(key[0]);
-        geometry = key[0];
-        key = key.slice(1);
-      }
-
-      for(var i=0; i<key.length; i++) {
-        if(isArray(key[i])) {
-          newKey.push(key[i]);
-        // If only a single point, not a range was emitted
-        } else {
-          newKey.push([key[i], key[i]]);
-        }
-      }
-      return {
-        key: newKey,
-        geometry: geometry
-      };
-    };
-
-    function within(key, start_range, end_range) {
-      var start;
-      var end;
-
-      for(var i=0; i<key.length; i++) {
-        start = key[i][0];
-        end = key[i][1];
-        if (
-          // Wildcard at the start
-          ((start_range[i] === null && (start <= end_range[i] || end_range[i] === null))
-           // Start is set
-           || (start <= end_range[i] || end_range[i] === null))
-          &&
-            // Wildcard at the end
-            ((end_range[i] === null && (end >= start_range[i] || start_range[i] === null))
-             // End is set
-             || (end >= start_range[i] || start_range[i] === null))) {
-          continue;
-        } else {
-          return false;
-        }
-      }
-      return true;
-    };
-
-    var emit = function(key, val) {
-      var keyGeom = normalizeKey(key);
-      var viewRow = {
-        id: current.doc._id,
-        key: keyGeom.key,
-        value: val,
-        geometry: keyGeom.geometry
-      };
-
-      // If no range is given, return everything
-      if (options.start_range !== undefined &&
+        // If no range is given, return everything
+        if (options.start_range !== undefined &&
           options.end_range !== undefined) {
-        if (!within(keyGeom.key, options.start_range, options.end_range)) {
-          return;
+          if (!within(keyGeom.key, options.start_range, options.end_range)) {
+            return;
+          }
         }
-      }
 
-      num_started++;
-      if (options.include_docs) {
-        //in this special case, join on _id (issue #106)
-        if (val && typeof val === 'object' && val._id){
-          db.get(val._id,
-              function(_, joined_doc){
+        num_started++;
+        if (options.include_docs) {
+          //in this special case, join on _id (issue #106)
+          if (val && typeof val === 'object' && val._id){
+            get(val._id).then(function(joined_doc){
                 if (joined_doc) {
                   viewRow.doc = joined_doc;
                 }
                 results.push(viewRow);
                 checkComplete();
               });
-          return;
-        } else {
-          viewRow.doc = current.doc;
+            return;
+          } else {
+            viewRow.doc = current.doc;
+          }
+        }
+        results.push(viewRow);
+      };
+
+      // ugly way to make sure references to 'emit' in map/reduce bind to the
+      // above emit
+      eval('fun = ' + fun.toString() + ';');
+
+      // exclude  _conflicts key by default
+      // or to use options.conflicts if it's set when called by db.query
+      var conflicts = ('conflicts' in options ? options.conflicts : false);
+
+      // only proceed once all documents are mapped and joined
+      function checkComplete() {
+        if (completed && results.length == num_started){
+          return success({rows: results});
         }
       }
-      results.push(viewRow);
-    };
 
-    // ugly way to make sure references to 'emit' in map/reduce bind to the
-    // above emit
-    eval('fun = ' + fun.toString() + ';');
-
-    // exclude  _conflicts key by default
-    // or to use options.conflicts if it's set when called by db.query
-    var conflicts = ('conflicts' in options ? options.conflicts : false);
-
-    // only proceed once all documents are mapped and joined
-    var checkComplete= function() {
-      if (completed && results.length == num_started){
-        return options.complete(null, {rows: results});
-      }
-    }
-
-    db.changes({
-      conflicts: conflicts,
-      include_docs: true,
-      onChange: function(doc) {
-        // Don't index deleted or design documents
-        if (!('deleted' in doc) && doc.id.indexOf('_design/') !== 0) {
-          current = {doc: doc.doc};
-          fun.call(this, doc.doc);
+      db.changes({
+        conflicts: conflicts,
+        include_docs: true,
+        onChange: function(doc) {
+          // Don't index deleted or design documents
+          if (!('deleted' in doc) && doc.id.indexOf('_design/') !== 0) {
+            current = {doc: doc.doc};
+            fun.call(this, doc.doc);
+          }
+        },
+        complete: function() {
+          completed= true;
+          checkComplete();
         }
-      },
-      complete: function() {
-        completed= true;
-        checkComplete();
-      }
+      });
     });
   }
 
-  function httpQuery(location, opts, callback) {
-
+  function httpQuery(location, opts) {
+    var request = denodify(db.request);
     // List of parameters to add to the PUT request
     var params = [];
 
@@ -175,44 +165,42 @@ function Spatial(db) {
 
     // We are referencing a query defined in the design doc
     var parts = location.split('/');
-    db.request({
+    return db.request({
       method: 'GET',
       url: '_design/' + parts[0] + '/_spatial/' + parts[1] + params
-    }, callback);
+    });
   }
-
-  function query(fun, opts, callback) {
+  function query(fun,opts,callback){
     if (typeof opts === 'function') {
       callback = opts;
       opts = {};
     }
-
-    if (callback) {
-      opts.complete = callback;
-    }
-
+    callback = callback || function(){};
     if (typeof fun !== 'string') {
       return callback ? callback({
-          status: 400,
-          error: 'invalid_request',
-          reason: 'Querying with a function is not supported for Spatial Views'
-        }) : undefined;
+        status: 400,
+        error: 'invalid_request',
+        reason: 'Querying with a function is not supported for Spatial Views'
+      }) : undefined;
     }
-
+    var result = spatialQuery(fun, opts);
+    result.then(function(resp){
+      if(typeof callback === 'function'){
+        callback(null,resp);
+      }
+    },callback);
+    return result;
+  }
+  function spatialQuery(fun, opts) {
     if (db.type() === 'http') {
-      return httpQuery(fun, opts, callback);
+      return httpQuery(fun, opts);
     }
 
     var parts = fun.split('/');
-    db.get('_design/' + parts[0], function(err, doc) {
-      if (err) {
-        if (callback) callback(err);
-        return;
-      }
-      viewQuery(doc.spatial[parts[1]], opts);
+    return get('_design/' + parts[0]).then(function(doc) {
+      return viewQuery(doc.spatial[parts[1]], opts);
     });
   }
-
   return {spatial: query};
 };
 function rotateCoords(coords){
@@ -236,12 +224,12 @@ function calculateBbox(geom) {
     // Merge all bounding boxes into one big one that encloses all
     return coords.reduce(function (acc, bbox) {
       return [
-        acc[0].map(function(a,i){
-          return Math.min(a,bbox[0][i]);
-        }),
-        acc[1].map(function(a,i){
-          return Math.max(a,bbox[1][i]);
-        })
+      acc[0].map(function(a,i){
+        return Math.min(a,bbox[0][i]);
+      }),
+      acc[1].map(function(a,i){
+        return Math.max(a,bbox[1][i]);
+      })
       ];
     });
   }
@@ -259,12 +247,12 @@ function calculateBbox(geom) {
       return [coord, coord];
     }
     return [
-      acc[0].map(function(a,i){
-        return Math.min(a,coord[i]);
-      }),
-      acc[1].map(function(a,i){
-        return Math.max(a,coord[i]);
-      })
+    acc[0].map(function(a,i){
+      return Math.min(a,coord[i]);
+    }),
+    acc[1].map(function(a,i){
+      return Math.max(a,coord[i]);
+    })
     ];
   }, null);
 };
