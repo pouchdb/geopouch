7221e812e6ab62b4d0b5e9d54e6e127a896fe56a
Calvin Metcalf <cmetcalf@appgeo.com>
2014-09-26 14:00:53 -0400

    working

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var Promise = require('lie');
-var RTree = reqire('async-rtree');
+var Promise = require('bluebird');
+var RTree = require('async-rtree');
 var calculatebounds = require('geojson-bounding-volume');
 //var createView = require('./create-view');
 //var Store = require('./store');
 
 
-exorts.spatial = function (fun, bbox, cb) {
+exports.spatial = function (fun, bbox, cb) {
+  if (bbox.length === 4) {
+    bbox = [[bbox[0], bbox[1]], [bbox[2], bbox[3]]];
+  }
   var db = this;
   var opts = {
     db: db,
@@ -13,26 +16,51 @@ exorts.spatial = function (fun, bbox, cb) {
     map: fun,
     temporary: true
   };
-  var func = new Function ('doc', 'emit', 'var func = (' + fun.toString().replace(/;\s*$/,"") + ');func(doc);');
-  var store = new RTree();
-  function addDoc(doc) {
-    var i = 0;
-    var promise = Promise.resolve(true);
-    var id = doc._id;
-    function emit(doc) {
-      var next = store.insert(id, calculatebounds(doc));
-      promise = promise.then(function () {
-        return next;
-      });
-    }
-    func(doc, emit);
-    return promise;
+  var idString;
+  if (typeof fun === 'function') {
+    idString = fun.toString();
+  } else {
+    idString = fun;
   }
-  return db.allDocs({includeDocs: true}).then(function (res) {
-    return Promise.all(res.rows.map(function (doc) {
-      return addDoc(doc);
-    })).then(function () {
-      return store.query(bbox, cb);
-    }, cb);
-  });
+  var store = new RTree();
+  return makeFunc(db, fun).then(function (func) {
+    function addDoc(doc) {
+      var i = 0;
+      var fulfill;
+      var promise = new Promise(function (f) {
+        fulfill = f;
+      });
+      var id = doc._id;
+      function emit(doc) {
+        fulfill(store.insert(id, calculatebounds(doc)));
+      }
+      func(doc, emit);
+      return promise;
+    }
+    return db.allDocs({include_docs: true}).then(function (res) {
+      return Promise.all(res.rows.filter(function (doc) {
+        if (!('deleted' in doc) && doc.id.indexOf('_design/') !== 0) {
+          return true;
+        }
+      }).map(function (doc) {
+        return addDoc(doc.doc);
+      }));
+    });
+  }).then(function () {
+    return store.query(bbox, true);
+  }).nodeify(cb);
+  
 };
+
+function makeFunc (db, fun) {
+  return new Promise (function (resolve, reject) {
+    if (typeof fun === 'function') {
+      return resolve(new Function ('doc', 'emit', 'var func = (' + fun.toString().replace(/;\s*$/,"") + ');func(doc);'));
+    }
+    var parts = fun.split('/');
+    resolve(db.get('_design/' + parts[0]).then(function (doc) {
+      var fun = doc.spatial[parts[1]];
+      return new Function ('doc', 'emit', 'var func = (' + fun.replace(/;\s*$/,"") + ');func(doc);');
+    }));
+  });
+}
